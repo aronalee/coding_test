fe1c0a2f5d3f9af962882ea26ed6344d38f972c3
dev-green-flamingo <devgflamingo@gmail.com>
2022-08-16 11:18:09 +0900

    [PROGRAMMERS] 2n 타일링
    - 리팩토링

---
+++ programmers/2n타일링.py
@@ -0,0 +1,10 @@
+# prob: https://school.programmers.co.kr/learn/courses/30/lessons/12900
+def solution(n):
+    memoization = [1, 2, 3]
+    if n <= len(memoization):
+        return memoization[n - 1]
+    answer = 0
+    for idx in range(3, n):
+        memoization[idx % 3] = (memoization[idx % 3 - 1] + memoization[idx % 3 - 2]) % 1_000_000_007
+        answer = memoization[idx % 3]
+    return answer
