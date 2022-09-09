88d857e9a97a9d97078ad5a942490de6dd02b714
dev-green-flamingo <devgflamingo@gmail.com>
2022-09-10 01:53:19 +0900

    [PROGRAMMERS] 네트워크
    - python으로 푼 문제를 java로 리팩토링
    - 네트워크 탐색과 네트워크 개수를 세는 부분을 분리

---
+++ programmers/네트워크/Solution.java
@@ -0,0 +1,41 @@
+// prob: https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class Solution {
+    boolean[] visited;
+    int[][] computers;
+
+    public int solution(int n, int[][] computers) {
+        this.visited = new boolean[n];
+        this.computers = computers;
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            answer++;
+            this.exploreNetwork(i);
+        }
+        return answer;
+    }
+
+    public void exploreNetwork(int startLocalhost) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startLocalhost);
+        while (queue.size() > 0) {
+            int localhost = queue.remove();
+            if (visited[localhost]) {
+                continue;
+            }
+            visited[localhost] = true;
+            for (int nextLocalhost = 0; nextLocalhost < computers[localhost].length; nextLocalhost++) {
+                if (!visited[nextLocalhost] && computers[localhost][nextLocalhost] == 1) {
+                    queue.add(nextLocalhost);
+                }
+            }
+        }
+    }
+}
