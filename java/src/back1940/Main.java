5f4f63e00240012e4fcb626792cc39c26b0e8e2b
dev-green-flamingo <devgflamingo@gmail.com>
2022-09-23 16:05:02 +0900

    [BACKJOON] 주몽
    - java로 해결
    - 재료들의 고유번호를 길이가 cost인 boolean 타입의 1d array에 저장
       - 재료들의 고유번호를 판단한다. 고유번호란 식별자의 역할을 하므로 2개 이상의 고유 번호를 가진 재료는 없다. 그러므로 boolean array로 판별 가능
       - cost 보다 크거나 같은 고유번호를 가진 재료는 쓸 필요가 없으르모 필터링
    - i번째의 배열과 cost-i의 배열의 boolean값이 모두 true이면 갑옷을 만들 수 있다고 판단
       - 배열을 모두 순회할 필요가 없으므로 절반만 순회

---
+++ java/src/back1940/Main.java
@@ -0,0 +1,49 @@
+// prob: https://www.acmicpc.net/problem/1940
+
+package back1940;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Back1940 {
+    private final int cost;
+    private final int[] materials;
+
+    public Back1940(int cost, int[] materials) {
+        this.cost = cost;
+        this.materials = materials;
+    }
+
+    public int countArmors() {
+        int count = 0;
+        boolean[] idArray = new boolean[cost];
+        for (int material : materials) {
+            if (material >= this.cost)
+                continue;
+            idArray[material] = true;
+        }
+        for (int i = 1; i < Math.ceil(idArray.length / 2.0); i++) {
+            if (idArray[i] && idArray[this.cost - i])
+                count++;
+        }
+        return count;
+    }
+}
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int countMaterial = Integer.parseInt(reader.readLine());
+        int cost = Integer.parseInt(reader.readLine());
+        int[] materials = new int[countMaterial];
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < countMaterial; i++)
+            materials[i] = Integer.parseInt(tokenizer.nextToken());
+        Back1940 back1940 = new Back1940(cost, materials);
+        int count = back1940.countArmors();
+        writer.write("" + count);
+        writer.flush();
+    }
+}
