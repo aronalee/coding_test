e5b72d39f60e7e317de9171fcedb04ec25ee9198
dev-green-flamingo <devgflamingo@gmail.com>
2022-09-23 18:02:27 +0900

    [BACKJOON] 좋다
    - java로 해결
    - 숫자들을 정렬하고 Good조건을 만족하는 수를 찾는다.
       - 정렬된 수를 제공한다는 조건이 없으므로 수행
    - startIdx번째 수와 endIdx번째 수(endIdx>startIdx) 수의 합이 숫자 배열의 K값과 일치하는지 확인
       - startIdx 는 0부터 시작
       - endIdx 는 배열 끝부터 시작
       - number의 수의 종류는 정수이므로 음수가 포함한다. 즉 매우 큰 양수와 매우 큰 음수가 합쳐서 Good조건을 만족하는 수가 될 수 있으므로 배열 끝에서 시작
          Ex) K: 10 number[i]=-1001 number[j]=1011
              number[i]+number[j]=-1001+1011=10=K
    - startIdx와 endIdx가 배열의 종단에서 시작하므로 목표하는 수의 인덱스인 idx와 일치하면 두 수를 합치는 연산을 수행하지 않고 통과
       - startIdx==idx
          - i++
       - endIdx==idx
          - j--

---
+++ java/src/back1253/Main.java
@@ -0,0 +1,62 @@
+// prob: https://www.acmicpc.net/problem/1253
+
+package back1253;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Back1253 {
+    private final int[] numbers;
+
+    public Back1253(int[] numbers) {
+        Arrays.sort(numbers);
+        this.numbers = numbers;
+    }
+
+    public int countGoodNumbers() {
+        int count = 0;
+        for (int idx = numbers.length - 1; idx >= 0; idx--) {
+            int dest = numbers[idx];
+            int startIdx = 0;
+            int endIdx = numbers.length - 1;
+            while (startIdx < endIdx) {
+                if (startIdx == idx) {
+                    startIdx++;
+                    continue;
+                } else if (endIdx == idx) {
+                    endIdx--;
+                    continue;
+                }
+                int result = numbers[startIdx] + numbers[endIdx];
+                if (result == dest) {
+                    count++;
+                    break;
+                } else if (result > dest) {
+                    endIdx--;
+                } else {
+                    startIdx++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int countNumbers = Integer.parseInt(reader.readLine());
+        int[] numbers = new int[countNumbers];
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < countNumbers; i++) {
+            numbers[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        Back1253 back1253 = new Back1253(numbers);
+        int count = back1253.countGoodNumbers();
+        writer.write("" + count);
+        writer.flush();
+    }
+}
