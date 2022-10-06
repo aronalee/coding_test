00740c438e5053b7237ed530a8bba8f62fb11cf9
dev-green-flamingo <devgflamingo@gmail.com>
2022-10-06 15:30:12 +0900

    [BACKJOON] 회의실 배정
    - JAVA로 해결
    - 그리디 알고리즘으로 수행
    - 다음의 우선 순위로 정렬
       1. 가장 먼저 끝나는 회의
       2. 가장 먼저 시작하는 회의
    - 먼저 끝나는 회의를 선택하면 이후의 회의 선택의 경우 수가 많다. 즉 먼저 끝나는 회의를 선택할 수록 더 많은 회의를 수행할 수 있다.
    - 회의 시작시간과 종료시간이 같은 경우가 있으므로 종료시간이 같을 경우 먼저 시작한 회의를 수행하여 많은 회의를 수행하도록 한다.

---
+++ java/src/backjoon/back1931/Main.java
@@ -0,0 +1,51 @@
+package backjoon.back1931;
+
+import java.io.*;
+import java.util.Arrays;
+
+class Node {
+    private final int start;
+    private final int end;
+
+    public Node(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(reader.readLine());
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            String[] tmp = reader.readLine().split(" ");
+            nodes[i] = new Node(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+        }
+        Arrays.sort(nodes, (node1, node2) -> {
+            if (node1.getEnd() != node2.getEnd())
+                return node1.getEnd() - node2.getEnd();
+            return node1.getStart() - node2.getStart();
+        });
+        int count = 0;
+        int end = -1;
+        for (Node node : nodes) {
+            if (node.getStart() < end)
+                continue;
+            end = node.getEnd();
+            count++;
+        }
+        writer.write(count + "\n");
+        writer.flush();
+    }
+}
