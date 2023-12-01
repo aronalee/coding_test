d62beaa8a3a7a5c857fbc29ccdc0861c9329fd72
aronalee <aronalee10@gmail.com>
2023-12-01 18:29:20 +0900

    [BACKJOON] 무한 문자열
    - 문자열 합치기로 같은 길이의 문자열로 만들고 비교
    - 문자의 최대 길이는 50자이며 최소 공배수도 50*49=2450 이므로 문자열 합치기 횟수도 작다

---
+++ java/src/random_depense/back12871/Back12871.java
@@ -0,0 +1,39 @@
+// prob: https://www.acmicpc.net/problem/12871
+
+package random_depense.back12871;
+
+public class Back12871 {
+
+    public boolean isContainStrings(String s1, String s2) {
+        return isContainStrInCompareStr(s1, s2) || isContainStrInCompareStr(s2, s1);
+    }
+
+    private boolean isContainStrInCompareStr(String str, String compareStr) {
+        if (str.length() < compareStr.length()) {
+            return false;
+        }
+        int lcm = getLCM(str.length(), compareStr.length());
+        String expendedStr = expendStringUntilLCM(str, lcm);
+        String expendedCompareStr = expendStringUntilLCM(compareStr, lcm);
+        return expendedStr.equals(expendedCompareStr);
+    }
+
+    private int getLCM(int strLength1, int strLength2) {
+        int gcd = getGCD(strLength1, strLength2);
+        return strLength1 * strLength2 / gcd;
+    }
+
+    private int getGCD(int strLength1, int strLength2) {
+        int gcd = 1;
+        for (int i = 1; i <= strLength1 && i <= strLength2; i++) {
+            if (strLength1 % i == 0 && strLength2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    private String expendStringUntilLCM(String str, int lcm) {
+        return str.repeat(Math.max(1, lcm / str.length()));
+    }
+}
---
+++ java/src/random_depense/back12871/Main.java
@@ -0,0 +1,27 @@
+// prob: https://www.acmicpc.net/problem/12871
+
+package random_depense.back12871;
+
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String s1 = reader.readLine();
+        String s2 = reader.readLine();
+        Back12871 back12871 = new Back12871();
+        boolean result = back12871.isContainStrings(s1, s2);
+        writeBuffer(result);
+        writer.flush();
+    }
+
+    private static void writeBuffer(boolean result) throws IOException {
+        if (result) {
+            writer.write("1");
+            return;
+        }
+        writer.write("0");
+    }
+}
