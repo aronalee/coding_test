927b9d08691fd2d41fab0ad47d77b34222fee364
aronalee <aronalee10@gmail.com>
2023-12-15 15:15:05 +0900

    [BACKJOON] n단 논법
    - 명제가 참이며 역은 거짓이다
    - 명제는 단방향으로 존재
    - 2가지의 구현 방법
       - linked list를 이용한 구현
       - hashmap을 이용한 구현
       - 이중 hashmap을 선택
    - 명제를 단방향으로 탐색하면서 question에서 알맞은 명제를 탐색
    - isVisted 집합을 만들어 이미 탐색한 곳에는 재방문하지 않도록 하여 사이클을 방지

---
+++ java/src/random_depense/back15723/Back15723.java
@@ -0,0 +1,36 @@
+// prob: https://www.acmicpc.net/problem/15723
+
+package random_depense.back15723;
+
+import java.util.*;
+
+public class Back15723 {
+    private final Map<String, List<String>> decisions;
+
+    public Back15723(Map<String, List<String>> decisions) {
+        this.decisions = decisions;
+    }
+
+    public boolean isPossible(String src, String dst) {
+        if (!decisions.containsKey(src)) {
+            return false;
+        }
+        Set<String> isVisited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>(decisions.get(src));
+        while (!queue.isEmpty()) {
+            String now = queue.remove();
+            if (now.equals(dst)) {
+                return true;
+            }
+            if (isVisited.contains(now)) {
+                continue;
+            }
+            isVisited.add(now);
+            if (!decisions.containsKey(now)) {
+                continue;
+            }
+            queue.addAll(decisions.get(now));
+        }
+        return false;
+    }
+}
---
+++ java/src/random_depense/back15723/Main.java
@@ -0,0 +1,54 @@
+// prob: https://www.acmicpc.net/problem/15723
+
+package random_depense.back15723;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int countDecisions = Integer.parseInt(reader.readLine());
+        Map<String, List<String>> decisions = inputPropositions(countDecisions);
+        int countQuestions = Integer.parseInt(reader.readLine());
+        List<List<String>> questions = inputQuestions(countQuestions);
+        Back15723 back15723 = new Back15723(decisions);
+        for (List<String> question : questions) {
+            if (back15723.isPossible(question.get(0), question.get(1))) {
+                writer.write("T\n");
+                continue;
+            }
+            writer.write("F\n");
+        }
+        writer.flush();
+    }
+
+    private static List<List<String>> inputQuestions(int countQuestions) throws IOException {
+        List<List<String>> questions = new LinkedList<>();
+        for (int i = 0; i < countQuestions; i++) {
+            String[] question = reader.readLine().split(" ");
+            questions.add(List.of(question[0], question[2]));
+        }
+        return questions;
+    }
+
+    private static Map<String, List<String>> inputPropositions(int countDecisions) throws IOException {
+        Map<String, List<String>> decisions = new HashMap<>();
+        for (int i = 0; i < countDecisions; i++) {
+            String[] decision = reader.readLine().split(" ");
+            decisions.putIfAbsent(decision[0], new LinkedList<>());
+            decisions.computeIfPresent(decision[0], (k, v) -> {
+                v.add(decision[2]);
+                return v;
+            });
+        }
+        return decisions;
+    }
+
+}
