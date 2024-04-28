8d213bd421c275e4563da17c04458615621b8d87
aronalee <aronalee10@gmail.com>
2024-04-28 19:59:55 +0900

    [BACKJOON] 번데기
    - 브루트포스 문제
    - 직접 패턴을 통해 타겟의 위치를 찾아내는 과정은 복잡함
    - t도 최대 10만 이므로 번데기 글자를 직접 만들면서 계산해도 시간 내에 해결이 가능하다고 판단
    - 번데기 글자를 만들면서 t에 위치되는 사람을 만나면 반복을 종료하고 반환
    - 시간복잡도: O(t)

---
+++ java/src/random_depnence/back15721/Back15721.java
@@ -0,0 +1,53 @@
+// prob: https://www.acmicpc.net/problem/15721
+
+package random_depnence.back15721;
+
+public class Back15721 {
+
+    private static final int SYMBOL_BYUNG = 0;
+
+    private static boolean isTargetPerson(int target, int symbol, int countByung, int countDegi) {
+        return symbol == 0 ? countByung == target : countDegi == target;
+    }
+
+    public int findPerson(int peopleTotal, int target, int symbol) {
+        int round = 1;
+        int personCurrent = 0;
+        int countByung = 0;
+        int countDegi = 0;
+        while (symbol == SYMBOL_BYUNG ? countByung < target : countDegi < target) {
+            for (int i = 0; i < 2; i++) {
+                countByung++;
+                if (isTargetPerson(target, symbol, countByung, countDegi)) {
+                    return personCurrent;
+                }
+                personCurrent = movePerson(peopleTotal, personCurrent);
+                countDegi++;
+                if (isTargetPerson(target, symbol, countByung, countDegi)) {
+                    return personCurrent;
+                }
+                personCurrent = movePerson(peopleTotal, personCurrent);
+            }
+            for (int i = 0; i <= round; i++) {
+                countByung++;
+                if (isTargetPerson(target, symbol, countByung, countDegi)) {
+                    return personCurrent;
+                }
+                personCurrent = movePerson(peopleTotal, personCurrent);
+            }
+            for (int i = 0; i <= round; i++) {
+                countDegi++;
+                if (isTargetPerson(target, symbol, countByung, countDegi)) {
+                    return personCurrent;
+                }
+                personCurrent = movePerson(peopleTotal, personCurrent);
+            }
+            round++;
+        }
+        return personCurrent;
+    }
+
+    private int movePerson(int peopleTotal, int personCurrent) {
+        return (personCurrent + 1) % peopleTotal;
+    }
+}
---
+++ java/src/random_depnence/back15721/Main.java
@@ -0,0 +1,22 @@
+// prob: https://www.acmicpc.net/problem/15721
+
+package random_depnence.back15721;
+
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int peopleTotal = Integer.parseInt(reader.readLine());
+        int target = Integer.parseInt(reader.readLine());
+        int symbol = Integer.parseInt(reader.readLine());
+        Back15721 back15721 = new Back15721();
+        int person = back15721.findPerson(peopleTotal, target, symbol);
+        writer.write(person + "\n");
+        writer.flush();
+    }
+}
