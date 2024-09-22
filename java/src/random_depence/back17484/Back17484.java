6df0824703d7924bfcb153eece3526da7cc4136a
aronalee <aronalee10@gmail.com>
2024-09-22 20:17:02 +0900

    [BACKJOON] 진우의 달 여행 (Small)
    - DP 문제
    - memoizaiton[h][w][dir_idx]: 달부터 행렬 위치가 (h,w) 까지 도달하였고, 전의 이동 방향이 dir_idx 일 때의 최소 비용
    - 다음에 이동할 수 있는 방향은 DIRECTIONS 배열로 이용하였으며, 이전의 배열 인덱스값과 비교하여 같은 방향으로 2번 움직이지 않도록 구성
        - DIRECTIONS=[-1,0,1]
    - dp(h,w,dir_idx)=Min(dp(h+1,w-1, 0),dp(h+1,w, 1),dp(h+1,w+1, 2))

---
+++ java/src/random_depence/back17484/Back17484.java
@@ -0,0 +1,56 @@
+// prob: https://www.acmicpc.net/problem/17484
+
+package random_depence.back17484;
+
+import java.util.Arrays;
+
+public class Back17484 {
+
+    private static final int EMPTY = -1;
+    private static final int VISITED_MOON = 0;
+    private static final int[] DIRECTIONS = new int[]{-1, 0, 1};
+
+    public int computeMinCost(int[][] map, int heightMax, int widthMax) {
+        int[][][] memoization = createMemoization(heightMax, widthMax);
+        for (int widthStart = 0; widthStart < widthMax; widthStart++) {
+            for (int directionIdx = 0; directionIdx < DIRECTIONS.length; directionIdx++) {
+                dp(map, memoization, 0, widthStart, directionIdx);
+            }
+        }
+        return Arrays.stream(memoization[0])
+            .flatMapToInt(Arrays::stream)
+            .filter(value -> value != EMPTY)
+            .min().orElse(EMPTY);
+    }
+
+    private int dp(int[][] map, int[][][] memoization, int height, int width, int beforeDirection) {
+        if (height>=map.length) {
+            return VISITED_MOON;
+        }
+        if (memoization[height][width][beforeDirection] != EMPTY) {
+            return memoization[height][width][beforeDirection];
+        }
+        int costMin=Integer.MAX_VALUE;
+        for (int directionIdx = 0; directionIdx < DIRECTIONS.length; directionIdx++) {
+            if (beforeDirection == directionIdx || width + DIRECTIONS[directionIdx] < 0 ||
+                width + DIRECTIONS[directionIdx] >= map[width].length) {
+                continue;
+            }
+            int result = dp(map, memoization, height + 1, width + DIRECTIONS[directionIdx],
+                directionIdx);
+            costMin = Math.min(costMin, result);
+        }
+        memoization[height][width][beforeDirection] = costMin+map[height][width];
+        return memoization[height][width][beforeDirection];
+    }
+
+    private int[][][] createMemoization(int heightMax, int widthMax) {
+        int[][][] memoization = new int[heightMax][widthMax][DIRECTIONS.length];
+        for (int[][] ints : memoization) {
+            for (int[] anInt : ints) {
+                Arrays.fill(anInt, EMPTY);
+            }
+        }
+        return memoization;
+    }
+}
---
+++ java/src/random_depence/back17484/Main.java
@@ -0,0 +1,34 @@
+// prob: https://www.acmicpc.net/problem/17484
+
+package random_depence.back17484;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(
+        new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(
+        new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String[] inputs = reader.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            inputs = reader.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(inputs[j]);
+            }
+        }
+        Back17484 back17484=new Back17484();
+        int minCost = back17484.computeMinCost(map,n,m);
+        writer.write(minCost + "\n");
+        writer.flush();
+    }
+}
